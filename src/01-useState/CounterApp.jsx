a30ff231dba52f26499e7777c5f724d31698abb0
VieriElCrack1 <santosvieri9@gmail.com>
2025-08-09 12:52:58 -0500

    feat: Iniciando con hooks - useState.

---
+++ src/01-useState/CounterApp.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const CounterApp = () => {
+
+    const [ {counter1, counter2, counter3} , setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+    });
+
+    return (
+        <>
+            <h1>Counter: {counter1}</h1>
+            <h1>Counter: {counter2}</h1>
+            <h1>Counter: {counter3}</h1>
+
+            <hr />
+
+            <button className="bg-black text-white p-[10px] rounded-2xl m-4 w-[60px] cursor-pointer"
+                onClick={() =>
+                    setCounter({
+                        counter1: counter1 + 1,
+                        counter2: counter2,
+                        counter3: counter3
+                    })
+                }>+1</button>
+        </>
+    )
+}
---
+++ src/HooksApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HooksApp = () => {
+  return (
+    <h1 className='text-2xl'>HooksApp</h1>
+  )
+}
